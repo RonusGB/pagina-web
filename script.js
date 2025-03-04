370459300218ddc773a82b1144552be4cb1f4770
Roman Gonzalez <romangonzalezburnet@gmail.com>
2025-03-04 19:11:46 -0300

    commit primer testeo

---
+++ script.js
@@ -0,0 +1,99 @@
+
+
+let numeron ={
+
+     num1: 1,
+     num2:"1",
+
+}
+
+let cadena=[hola,chau]
+
+let hola = 1;
+while (hola<5){
+    hola++;
+    document.write(hola)
+}
+
+for (let i=123; i==130; i++){
+    document.write(i)
+}
+"continue es para saltear ese ciclo de for"
+
+"in devuelve la posicion"
+"of devuelve el valor"
+
+
+let mensaje=prompt("hola, esto es un input");
+console.log(num1===num2);
+mensaje--; 
+alert(`${num1}${num2}holadsadasd`);
+if (num1===num2)
+    {alert("hola")}
+else if (num1!==num2)
+    {alert("piola")}
+document.write("nashe")
+
+
+return
+
+
+
+
+
+"comprobar si comienzan las cadenas con el mismo caracter"
+
+let Cadena="hola";
+
+resultado= "h".startsWith(Cadena)
+
+
+class plano{
+
+    constrctor(color,cantidad,calidad){
+        this.color=color;
+        this.cantidad=cantidad;
+        this.calidad=calidad;}
+}
+
+
+class plano_premium extends plano{
+
+    constrctor(color,cantidad,calidad,tamanio){
+        super(color,cantidad,calidad)
+        this.tamanio=tamanio;
+    }
+    static saludar(){
+        alert("hola");
+    }
+}
+
+saludo.plano_premium();
+
+"static hace que se ejecute a pesar de que HOLA no este creado"
+
+
+"los objetos se definen con const"
+
+`get  y set sirven para nsdglnsdfpo y se llaman asi 
+
+asd.saludo="infoasdasd";
+
+en vez de asi
+asd.saludo("infoasdasd")
+
+`
+
+const color= new plano(rojo,3,"alta");
+
+
+
+                "ARREGLOS"
+
+".pop() borra el ultimo elemento y si usas un write te lee el elemento borrado"
+".push() son los elementos que tiene el arreglo"
+".reverse() invierte el orden de los elementos "
+".shift() elimina el primero"
+".unshift() agrega 1 o mas elementos al inicio del arreglo y nos devuelve la longitud nueva"
+".short() los ordena alfabeticamente a los elementos"
+".splice(posicion,elementos borrados,elementos agregados)"
